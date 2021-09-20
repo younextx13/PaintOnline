38a6f42ac0552b70ff67c261df4634c30c00d97c
younext13 <sasha021303@gmail.com>
2021-09-20 17:53:19 +0300

    add range input

--- main.js
+++ main.js
@@ -1,39 +1,31 @@
-let
-    canv = document.getElementById('canvas');
-    ctx  = canv.getContext('2d');
+const canv = document.getElementById("canvas"); ctx = canv.getContext("2d"); 
+let isMouseDown = null;
 
-canv.width = 1000;
-canv.height = 700;
-
-//Code
-
-canv.addEventListener('mousedown', function(){
-    isMouseDown = true;
+canv.addEventListener("mousedown", function () {
+  isMouseDown = true;
 });
 
-canv.addEventListener('mouseup', function(){
-    isMouseDown = false;
+canv.addEventListener("mouseup", function () {
+  isMouseDown = false;
+  ctx.beginPath();
+});
+
+let width_line = 5; color_line = "red";
+
+canv.addEventListener("mousemove", function (e) {
+  let rect = canvas.getBoundingClientRect(); 
+  ctx.lineWidth = width_line * 2;
+  if (isMouseDown) {
+    ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
+    ctx.strokeStyle = color_line;
+    ctx.stroke();
+
     ctx.beginPath();
-});
+    ctx.arc(e.clientX - rect.left, e.clientY - rect.top, width_line, 0, Math.PI * 2);
+    ctx.fillStyle = color_line;
+    ctx.fill();
 
-let 
-    width_line = 10;
-    color_line = 'red';
-
-canv.addEventListener('mousemove', function(e){
-    ctx.lineWidth = width_line * 2;
-    if(isMouseDown)
-    {
-        ctx.lineTo(e.clientX, e.clientY);
-        ctx.strokeStyle = color_line;
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.arc(e.clientX, e.clientY, width_line, 0, Math.PI * 2);
-        ctx.fillStyle = color_line;
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
-    }
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
+  }
 });
